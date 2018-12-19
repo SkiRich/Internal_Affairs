33219be99922721e40fd4a9f21b0b71ef09d489e
SkiRich <richard@szkodzinski.com>
2018-12-19 13:11:42 -0500

    Add Renegade to Curable Traits
    - For sanatorium

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,10 @@
 # Changelog for Internal Affairs
+## v0.02 12/19/2018 1:10:25 PM
+
+#### Added
+- Renegade Curable trait to Sanatorium
+
+--------------------------------------------------------
 ## v0.01 12/18/2018 1:11:27 AM
 
 - Initial Commit
--- Code/IA_1Init.lua
+++ Code/IA_1Init.lua
@@ -5,7 +5,41 @@
 -- Created Dec 17th, 2018
 -- Updated Dec 17th, 2018
 
-local lf_print = false -- Setup debug printing in local file
+local lf_print = true -- Setup debug printing in local file
                        -- Use if lf_print then print("something") end
 
 local ModDir = CurrentModPath
+
+
+
+-- adds additional traits to Sanatorium for curing
+local function IAaddCures()
+	-- add Renegade,
+	local curableTraits = g_SanatoriumTraits
+  local found = false
+
+  for i = 1, #curableTraits do
+  	if curableTraits[i] == "Renegade" then
+  		found = true
+  		if lf_print then print("Found Renegade curable trait") end
+  		break
+  	end -- if curableTraits
+  end -- for i
+
+	if not found then
+		if lf_print then print("Renegade cure not found.  Adding cure.") end
+		table.insert(curableTraits, "Renegade")
+	end -- if not found
+end -- function IAaddCures()
+
+
+
+-------------------------------------------- OnMsgs --------------------------------------------------
+
+function OnMsg.CityStart()
+	IAaddCures()
+end -- OnMsg.CityStart()
+
+function OnMsg.LoadGame()
+	IAaddCures()
+end -- OnMsg.LoadGame()
